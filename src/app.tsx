9f464c93ec24098fd09ef7d8212e4863341a264d
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-11-25 19:45:19 +0300

    feat: assets and load fonts

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <h1 className="text-5xl text-orange-500 font-bold">
+        Welcome to Awwards
+      </h1>
+    </main>
   )
 }
 
