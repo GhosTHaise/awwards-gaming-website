45e8f2a24bdf3bd68c5d40efa3ff32807b94eaf1
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-11-25 19:54:51 +0300

    feat: load fonts

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">
+      <h1 className="text-5xl text-orange-500 font-bold font-robert-medium">
         Welcome to Awwards
       </h1>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ export default {
   theme: {
     extend: {
         fontFamily : {
-          zentry : ['ZenTry', 'sans-serif'],
-          general : ['General', 'sans-serif'],
+          zentry : ['zenTry', 'sans-serif'],
+          general : ['general', 'sans-serif'],
           'circular-web': ["circular-web", "sans-serif"],
           'robert-medium' : ["robert-medium", "sans-serif"],
           'robert-regular' : ["robert-regular", "sans-serif"],
