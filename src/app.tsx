c8f4c9fd007a4614aa4e5987f955b860d3619323
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-11-26 19:24:14 +0300

    feat: extend colors

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold font-robert-medium">
+      <h1 className="text-5xl text-violet-300 font-bold font-robert-medium">
         Welcome to Awwards
       </h1>
     </main>
