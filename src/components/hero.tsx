f79ea624a84da90f12d3f88069aa3b2d24615e1b
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-11-27 19:42:58 +0300

    feat : button

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,4 +1,6 @@
 import { useRef, useState } from "preact/hooks"
+import Button from "./shared/button";
+import { TiLocationArrow } from "react-icons/ti";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1)
@@ -67,7 +69,22 @@ const Hero = () => {
         </h1>
 
         <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              redifi<b>n</b>e
+            </h1>
 
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the Metagame Layer <br /> Unleash the Play Economy
+            </p>
+
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="!bg-yellow-300 flex-center gap-1"
+            />
+          </div>
         </div>
       </div>
     </section>
