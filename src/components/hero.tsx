a504cb6c49aa7e55d5dcf45b241206223ac7aa08
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-11-27 19:47:21 +0300

    feat : install gsap

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -87,6 +87,10 @@ const Hero = () => {
           </div>
         </div>
       </div>
+
+      <h1 className="special-font hero-heading absolute bottom-5 right-5  text-black">
+          G<b>a</b>ming
+        </h1>
     </section>
   )
 }
