f91a581b269ef153ea9726d0f58c39f712f13dd5
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-11-30 19:23:20 +0300

    feat : create navbar components

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,10 @@
 import Hero from "./components/hero"
 import About from "./components/about"
+import Navbar from "./components/navbar"
 const App = () => {
   return (
     <main className={"relative min-h-screen w-screen overflow-x-hidden"}>
+      <Navbar />
       <Hero />
       <About />
       <section className="z-0 min-h-screen bg-blue-500" />
