38ecbe127c9dee57041829b93357c719975d501e
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-12-02 19:42:19 +0300

    fix : build error

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -92,20 +92,20 @@ const Features = () => {
           <div className="bento-tilt_2">
             <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
               <h1 className="bento-title special-font max-w-64 text-black">M<b>o</b>re co<b>m</b>ing s<b>o</b>on!</h1>
-              <TiLocationArrow
-                className="m-5 scale-[5] self-end"
-              />
+              <div className="m-5 scale-[5] self-end">
+                <TiLocationArrow />
+              </div>
             </div>
           </div>
 
           <div className="bento-tilt_2">
-              <video 
-                src="videos/feature-5.mp4"
-                loop
-                muted
-                autoPlay
-                className="size-full object-cover object-center"
-              />
+            <video
+              src="videos/feature-5.mp4"
+              loop
+              muted
+              autoPlay
+              className="size-full object-cover object-center"
+            />
           </div>
         </div>
       </div>
