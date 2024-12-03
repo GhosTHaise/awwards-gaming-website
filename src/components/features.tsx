358eca4963d9bac23d334d57e84d786e60187d22
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-12-03 20:05:28 +0300

    feat : tilt animation

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "preact/compat"
+import { ReactNode, useRef, useState } from "preact/compat"
 import { TiLocationArrow } from "react-icons/ti";
 
 type BentoCardProps = {
@@ -12,10 +12,38 @@ type BentoTiltProps = {
   className: string;
 }
 
-const BentoTilt = ({children, className} : BentoTiltProps) => {
+const BentoTilt = ({ children, className }: BentoTiltProps) => {
+  const [transformStyle, setTransformStyle] = useState("");
+  const itemRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseMove = (e: MouseEvent) => {
+    if(!itemRef.current) return;
+
+    const { left, top, width, height } = itemRef.current.getBoundingClientRect();
+
+    const relativeX = (e.clientX - left) / width;
+    const relativeY = (e.clientY - top) / height;
+
+    const tiltX = (relativeY - 0.5) * 5;
+    const tiltY = (relativeX - 0.5) * 5;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.98, 0.98, 0.98)`;
+
+    setTransformStyle(newTransform);
+  }
+
+  const handleMouseLeave = (e: MouseEvent) => {
+    setTransformStyle('');
+  }
+
   return (
-    <div className={className}>
-        {children}
+    <div
+      ref={itemRef}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{transform : transformStyle}}
+      className={`transition-all duration-75 ${className}`}>
+      {children}
     </div>
   )
 }
